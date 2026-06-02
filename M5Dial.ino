4e4d9fa92d4c6c904e9b11c4ef33e122f79bc921
Beserrovsky <felipebeserra25@gmail.com>
2026-06-01 23:39:57 -0300

    Stuck Spinner

---
+++ M5Dial.ino
@@ -0,0 +1,77 @@
+#include "M5Dial.h"
+#include <Arduino.h>
+#include <lvgl.h>
+#include <Wire.h>
+#include <SPI.h>
+#include "ui.h"
+
+// init the tft espi
+static lv_disp_draw_buf_t draw_buf;
+static lv_disp_drv_t disp_drv;  // Descriptor of a display driver
+
+#define EXAMPLE_LCD_H_RES 240
+#define EXAMPLE_LCD_V_RES 240
+#define LV_VER_RES_MAX 240
+#define LV_HOR_RES_MAX 240
+M5GFX *tft;
+
+long oldPosition = -999;
+
+void tft_lv_initialization() {
+  lv_init();
+  static lv_color_t buf1[(LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10];  // Declare a buffer for 1/10 screen siz
+  static lv_color_t buf2[(LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10];  // second buffer is optionnal
+  // Initialize `disp_buf` display buffer with the buffer(s).
+  lv_disp_draw_buf_init(&draw_buf, buf1, buf2, (LV_HOR_RES_MAX * LV_VER_RES_MAX) / 10);
+  tft=&M5Dial.Lcd;
+}
+
+// Display flushing
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+  tft->startWrite();
+  tft->setAddrWindow(area->x1, area->y1, w, h);
+  tft->pushColors((uint16_t *)&color_p->full, w * h, true);
+  tft->endWrite();
+  lv_disp_flush_ready(disp);
+}
+
+void init_disp_driver() {
+  lv_disp_drv_init(&disp_drv);  // Basic initialization
+  disp_drv.flush_cb = my_disp_flush;  // Set your driver function
+  disp_drv.draw_buf = &draw_buf;      // Assign the buffer to the display
+  disp_drv.hor_res = LV_HOR_RES_MAX;  // Set the horizontal resolution of the display
+  disp_drv.ver_res = LV_VER_RES_MAX;  // Set the vertical resolution of the display
+  lv_disp_drv_register(&disp_drv);                   // Finally register the driver
+  lv_disp_set_bg_color(NULL, lv_color_hex3(0x000));  // Set default background color to black
+}
+
+void setup()
+{
+  auto cfg = M5.config();
+  M5Dial.begin(cfg, true, false);
+  M5Dial.Display.setBrightness(80);
+  Serial.begin(115200);
+  tft_lv_initialization();
+  init_disp_driver();
+  ui_init();
+}
+
+
+
+void loop()
+{
+    uint32_t wait_ms = lv_timer_handler();
+    M5.delay(wait_ms);  
+
+    M5Dial.update();
+    long newPosition = M5Dial.Encoder.read();
+    if (newPosition != oldPosition) {
+        M5Dial.Speaker.tone(8000, 20);
+        oldPosition = newPosition;
+    }
+
+}
+
+
