1dc732c2b8f1a29769c605540ee44fb2c5f168bb
Beserrovsky <felipebeserra25@gmail.com>
2026-06-02 19:26:17 -0300

    Clean and compiling

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,10 @@
 
 void setup()
 {
-    auto cfg = M5.config();
-    M5.begin(cfg);
-
-    Serial.begin(115200);
-
-    M5.Display.fillScreen(BLACK);
-
-    M5.Display.setCursor(20,20);
-    M5.Display.print("Hello M5Dial");
+  
 }
 
 void loop()
 {
-    M5.update();
+
 }
